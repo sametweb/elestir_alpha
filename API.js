362d335a2e96d1dcdc75938f44c7f968f27a8e5a
sametweb <sametmutevellioglu@gmail.com>
2019-12-24 23:45:07 -0800

    createquestion API request  added

--- API.js
+++ API.js
@@ -5,7 +5,8 @@ const baseURL = "https://elestirorg.appspot.com";
 const actions = {
   login: "/login",
   signup: "/signup",
-  getquestions: "/getquestions"
+  getquestions: "/getquestions",
+  createquestion: "/createquestion"
 };
 
 const headers = {
@@ -20,6 +21,8 @@ export const PostRequest = (action, data) => {
       return axios.post(`${baseURL}${actions.signup}`, data, headers);
     case "getquestions":
       return axios.get(`${baseURL}${actions.getquestions}`, data, headers);
+    case "createquestion":
+      return axios.post(`${baseURL}${actions.createquestion}`, data, headers);
     default:
       console.log("Please define an action");
   }
