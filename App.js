b725c2ef477ca030da50ae6e97de27545cf31da4
sametweb <sametmutevellioglu@gmail.com>
2019-12-30 22:29:16 -0800

    fix

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 //Navigation
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
